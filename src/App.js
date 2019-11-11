e4332cdeaf94d56d6cbfbb48e61e66f25616e005
bulbulahmed9 <bulbulahmed.tk@gmail.com>
2019-11-10 17:23:25 -0800

    person list updated

--- src/App.js
+++ src/App.js
@@ -1,26 +1,59 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const Person = (props) => {
+  const { img, name, job } = props.person;
+  const { children } = props;
+  const url = `https://randomuser.me/api/portraits/thumb/men/${img}.jpg`
+  return(
+    <div className="person">
+      <img src={url} alt="img"/>
+      <div>
+        <h4>{name}</h4>
+        <h4>{job}</h4>
+        {children}
+      </div>
     </div>
-  );
+  )
 }
+
+const PersonList = () => {
+
+  const people = [
+    {
+      img: 10,
+      name: 'John',
+      job: 'Developer'
+    },
+    {
+      img: 11,
+      name: 'Doe',
+      job: 'Designer'
+    },
+    {
+      img: 14,
+      name: 'Jovan',
+      job: 'Fullstack Developer'
+    },
+    {
+      img: 25,
+      name: 'Jov',
+      job: 'Singer'
+    }
+  ]
+
+  return(
+    <section>
+      <Person person={people[0]} />
+      <Person person={people[1]} />
+      <Person person={people[3]} />
+      <Person person={people[2]} >
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores explicabo minima odio eos possimus, suscipit aut obcaecati deserunt a delectus repellendus sunt reiciendis rerum exercitationem, iure ad eius hic? In.
+      </Person>
+    </section>
+  )
+}
+
+const App = () => (<PersonList />)
 
 export default App;
